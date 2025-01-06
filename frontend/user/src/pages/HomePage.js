6a539611936e02f076339b4408a426bd2adef85f
ngocg-0129 <tranminhngoc_t67@hus.edu.vn>
2025-01-06 18:46:31 +0700

    add user
    Update App.css and add components

---
+++ frontend/user/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header"
+import Banner from "../components/Banner/Banner"
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <Banner />
+        </div>
+            
+        
+    )
+}
+
+export default HomePage
