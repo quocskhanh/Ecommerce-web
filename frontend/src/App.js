f7209d1d7b75c48d4d6f58ba6233056b47bf4b43
ngocg-0129 <tranminhngoc_t67@hus.edu.vn>
2025-01-17 19:01:20 +0700

    cm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
-<<<<<<< HEAD
-=======
-import AccountPage from "./pages/auth/AccountPage";
-=======
 import { lazy, Suspense, useEffect, useState } from "react";
->>>>>>> 361b2a012c3c43d33ec9ebc2fdb563222f5a4a43
-import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
->>>>>>> 78183594a64d056bd4b191437ed12568d40c8f78
 import CategoriesPage from "./pages/category/CategoriesPage";
+import ReportPage from "./pages/report/ReportPage";
+import SettingPage from "./pages/SettingPage";
 import LogoutPage from "./pages/LogoutPage";
 import AddProductPage from "./pages/product/AddProductPage";
 
 import AddCategoryPage from "./pages/category/AddCategoryPage";
 import AccountsPage from "./pages/account/AccountsPage";
-<<<<<<< HEAD
-=======
-import AccountPage from "./pages/auth/AccountPage";
-import ShippingPage from "./pages/shipping/ShippingPage";
-<<<<<<< HEAD
-import UpdateShippingPage from "./pages/shipping/UpdateShippingPage";
-=======
+import AddAccountPage from "./pages/account/AddAccountPage";
 import AddOrderPage from "./pages/order/AddOrderPage";
 import EditCategoryPage from "./pages/category/EditCategoryPage";
->>>>>>> 78183594a64d056bd4b191437ed12568d40c8f78
 import AccountPage from "./pages/auth/AccountPage";
-import ShippingPage from "./pages/shipping/ShippingPage";
+
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ShopPage from "./pages/ShopPage";
+import HomePage from "./pages/HomePage";
+import CheckOutPage from './pages/CheckOutPage';
+import ProductPageUser from './pages/ProductPage';
+import { CartProvider } from "./components/ShopPage/ListProducts/CartContext";
+
+
+import Login from "./pages/auth/Login";
+import AccountPageUser from "./pages/AccountPageUser";
+import LogOutUser from "./pages/LogOutUser";
+
 
 // Lazy load components
 const ProductPage = lazy(() => import("./pages/product/ProductPage"));
@@ -34,30 +33,57 @@ const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 const OrderPage = lazy(() =>import( "./pages/order/OrderPage"))
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    setIsLoggedIn(false);
+  };
+
   return (
+    <CartProvider>
         <Suspense>
           <Routes>
-<<<<<<< HEAD
+            <Route path="/shop" element={<ShopPage />} /> 
             {/* Các route dành cho quản trị viên */}
             <Route path="/admin/*" element={<DashboardPage />} />
-=======
-            {/* Các route chính */}
-           <Route path="/admin/*" element={<DashboardPage />} />
->>>>>>> 78183594a64d056bd4b191437ed12568d40c8f78
-            <Route path="/admin/shipping" element={<ShippingPage />} />
             <Route path="/auth/*" element={<AccountPage />} />
             <Route path="/admin/product/*" element={<ProductPage />} />
             <Route path="/admin/order/*" element={<OrderPage />} />
             <Route path="/admin/categories/*" element={<CategoriesPage />} />
+            <Route path="/admin/categories/:id" element={<EditCategoryPage />} />
             <Route path="/admin/accounts/*" element={<AccountsPage />} />
+            <Route path="/admin/accounts/add-account" element={<AddAccountPage />} />
+            <Route path="/admin/setting" element={<SettingPage />} />
             <Route path="/admin/logout" element={<LogoutPage />} />
+            <Route path="/admin/reports" element={<ReportPage />} />
             <Route path="/admin/product/add-product" element={<AddProductPage />} />
             <Route path="/admin/categories/add-category" element={<AddCategoryPage/>} />
+            <Route path="/admin/order/add-order" element={<AddOrderPage/>} />
 
 
 
+
+
+
+            <Route path="/" element={<HomePage />} /> 
+            {/* <Route path="/shop" element={<ShopPage />} />  */}
+            <Route path="/product/:id" element={<ProductPageUser />} /> 
+            <Route path="/checkout" element={<CheckOutPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/account" element={<AccountPageUser />} />
+            <Route path="/log-out" element={<LogOutUser />} />
           </Routes>
         </Suspense>
+      </CartProvider>
   );
 }
 
